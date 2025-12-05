bfa8fad87d919ad33ba4f4a29e455ca4bda3d24f
namgal <gal@nigzu.com>
2025-12-05 14:45:23 +0800

    单文件中的ASP.NET应用测试

---
+++ HelloAPI.cs
@@ -0,0 +1,11 @@
+#!/usr/bin/env dotnet
+#:sdk Microsoft.NET.Sdk.Web
+#:property PublishAot=false
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8500");
+
+var app = builder.Build();
+app.MapGet("/", (string? query) => $"你好,{query ?? ""}");
+
+app.Run();
